80828ea080bc376b3fbe522496c48633a10dc52e
ydde <salimmchomvu7@gmail.com>
2023-12-15 19:40:47 +0300

    we gonn be there

--- app/Livewire/AdminPages/AdminPostCreator.php
+++ app/Livewire/AdminPages/AdminPostCreator.php
@@ -2,11 +2,81 @@
 
 namespace App\Livewire\AdminPages;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AdminPostCreator extends Component
 {
+    use WithPagination;
+    public $post_title;
+    public $search;
+    public $post_description;
+    public $editingPostID;
+    public $showEditTab = false;
+    public function createPost(){
+        $validated = $this->validate([
+            'post_title'=>'required|string',
+            'post_description'=>'required'
+        ]);
+
+        $post = Post::latest()->first();
+
+        if ($post){
+            $code = $post->post_code + 3;
+            $post_code = str_pad($code, 4,0,STR_PAD_LEFT);
+
+        }else{
+            $code = 0;
+            $code = $code + 3;
+            $post_code = str_pad($code, 4,0,STR_PAD_LEFT);
+
+        }
+
+        Post::create([
+            'post_title'=>$this->post_title,
+            'description'=>$this->post_description,
+            'post_code'=>$post_code,
+        ]);
+
+        $this->reset('post_title','post_description');
+        session()->flash('success','Post added Successfully');
+
+    }
+    public function editPost($postId){
+        $post = Post::where('id', $postId)->first();
+        $this->post_title = $post->post_title;
+        $this->post_description = $post->description;
+
+        $this->editingPostID = $postId;
+
+        $this->showEditTab = true;
+
+    }
+
+    public function updatePost($id){
+        $validated = $this->validate([
+            'post_title'=>'required|string',
+            'post_description'=>'required'
+        ]);
+
+        $post = Post::find($id);
+        $post->update([
+            'post_title'=>$this->post_title,
+            'description'=>$this->post_description,
+            'post_code'=>$post->post_code
+        ]);
+
+        $this->reset('post_title','post_description');
+        session()->flash('success','Post Updated Successfully');
+
+        $this->showEditTab = false;
+
+
+
+    }
+
     public function logout(){
         Auth::logout();
 
@@ -18,6 +88,9 @@ class AdminPostCreator extends Component
     }
     public function render()
     {
-        return view('livewire.admin-pages.admin-post-creator');
+        $posts = Post::latest()->where('post_title','like',"%{$this->search}%")->paginate(10);
+        return view('livewire.admin-pages.admin-post-creator',[
+            'posts'=>$posts
+        ]);
     }
 }
--- resources/views/livewire/admin-pages/admin-post-creator.blade.php
+++ resources/views/livewire/admin-pages/admin-post-creator.blade.php
@@ -6,25 +6,66 @@
         </button>
     </div>
     <div class="flex h-full items-center flex-col bg-blue-400">
-        <span class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-8">Create Post</span>
-        <div class="flex bg-gray-500 mt-8 rounded-md w-2/3 p-4 ">
-            <form class="max-w-sm mx-auto w-full mt-6 mb-6">
-                <div class="mb-5">
-                    <label for="postTitle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
-                    <input type="text" id="postTitle" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
-                </div>
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write post description here..."></textarea>                </textarea>
+        @if(!$this->showEditTab)
+            <span class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-8">Create Post</span>
 
-                <button type="submit" class="mt-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Post</button>
-            </form>
+        @endif
+        @if($this->showEditTab)
+            <span class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-8">Edit Post</span>
+
+        @endif
+        <div class="flex bg-gray-500 mt-8 rounded-3xl w-2/3 p-4 ">
+           @if(!$this->showEditTab)
+                <form wire:submit="createPost" class="max-w-sm mx-auto w-full mt-6 mb-6">
+                    <div class="mb-5">
+                        <label for="post_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
+                        <input wire:model="post_title" type="text" id="post_title" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
+                        @error('post_title')
+                        <span class=" rounded-md  text-red-400 text-xs p-2 w-64">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="">
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <textarea wire:model="post_description" id="post_description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write post description here..."></textarea>
+                        @error('post_description')
+                        <span class=" rounded-md  text-red-400 text-xs p-2 w-64">{{ $message }}</span>
+                        @enderror
+
+                    </div>
+
+                    <button type="submit" class="mt-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Post</button>
+                </form>
+           @endif
+            @if($this->showEditTab)
+                    <form wire:submit="updatePost({{ $this->editingPostID }})" class="max-w-sm mx-auto w-full mt-6 mb-6">
+                        <div class="mb-5">
+                            <label for="post_title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
+                            <input wire:model="post_title" type="text" id="post_title" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
+                            @error('post_title')
+                            <span class=" rounded-md  text-red-400 text-xs p-2 w-64">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="">
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                            <textarea wire:model="post_description" id="post_description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write post description here..."></textarea>
+                            @error('post_description')
+                            <span class=" rounded-md  text-red-400 text-xs p-2 w-64">{{ $message }}</span>
+                            @enderror
+
+                        </div>
+
+                        <button type="submit" class="mt-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update Post</button>
+                    </form>
+
+            @endif
+
 
         </div>
         <div class="flex flex-col w-full">
             <span class=" ml-6 text-sm font-medium text-gray-900 dark:text-white flex mt-20">Posts List</span>
             <div class="p-6">
                 <div>
-                    <input id="filters" type="search" placeholder="Search post..." class="bg-gray-50 xs:text-xs mb-4 w-56 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                    <input wire:model.live.debounce.200ms="search" id="search" type="search" placeholder="Search post..." class="bg-gray-50 xs:text-xs mb-4 w-56 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                 </div>
 
                 <div class="relative overflow-x-auto xs:rounded-lg shadow-md sm:rounded-lg">
@@ -41,7 +82,7 @@
                                 Post Code
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Created At
+                                Description
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Action
@@ -50,25 +91,28 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                1
-                            </th>
-                            <td class="px-6 py-4">
-                                Salim Iddi Mchomvu
-                            </td>
-                            <td class="px-6 py-4">
-                                33848_4378
-                            </td>
-                            <td class="px-6 py-4">
-                                Voted
-                            </td>
-                            <td class="px-6 py-4">
-                                <button class="flex rounded-md p-2 bg-blue-400 text-white hover:bg-blue-700">Edit Post</button>
-                            </td>
+                        @foreach($posts as $post)
+                            <tr wire:key="{{ $post->id }}" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $loop->index+1 }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $post->post_title }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $post->post_code }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $post->description }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <button wire:click="editPost({{ $post->id }})" class="flex rounded-md p-2 bg-blue-400 text-white hover:bg-blue-700">Edit Post</button>
+                                </td>
 
 
-                        </tr>
+                            </tr>
+
+                        @endforeach
 
                         </tbody>
                     </table>
